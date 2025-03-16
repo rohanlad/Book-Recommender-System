1fcd47ae9aa6522975e20ed29f79a9a916fc4cba
Rohan Lad <41441712+rohanlad@users.noreply.github.com>
2025-03-16 17:21:45 +0000

    clean

--- cars_books.py
+++ cars_books.py
@@ -1,7 +1,6 @@
 # *********************
 # CITATIONS
 # *********************
-# A lot of this code is adapted from the Tensorflow guides & tutorials. 
 # https://www.tensorflow.org/recommenders/examples/basic_retrieval
 # https://www.tensorflow.org/recommenders/examples/basic_ranking
 # https://www.tensorflow.org/recommenders/examples/featurization
@@ -250,9 +249,7 @@ def rank(train, test):
   model_rank = RankingMain()
   model_rank.compile(optimizer=tf.keras.optimizers.Adagrad(learning_rate=0.01))
   model_rank.fit(train.batch(8192), epochs=100)
-  print('----------------------carseval')
   print(model_rank.evaluate(test.batch(8192), return_dict=True))
-  print('----------------------carseval')
   model_rank.save_weights('cars_save_rank/')
   return
 
